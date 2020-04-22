64fdcd33a3d0f0d7f8a16a7428889d4c40424e11
prestonchao <preston7@illinois.edu>
2020-04-21 20:26:07 -0500

    Added usage of Box2D Library

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -2,7 +2,14 @@
 
 #include "my_app.h"
 
+#include <cinder/Font.h>
+#include <cinder/Text.h>
+#include <cinder/Vector.h>
+#include <cinder/audio/audio.h>
+#include <cinder/gl/draw.h>
+#include <cinder/gl/gl.h>
 #include <cinder/app/App.h>
+
 #include <Box2D/Box2D.h>
 
 namespace myapp {
@@ -12,8 +19,34 @@ using cinder::app::KeyEvent;
 MyApp::MyApp() { }
 
 void MyApp::setup() {
+  cinder::gl::enableDepthWrite();
+  cinder::gl::enableDepthRead();
+
+  // Create world in Box2D
+
+  // Define the gravity vector.
   b2Vec2 gravity(0.0f, -10.0f);
+
+  // Construct a world object, which will hold and simulate the rigid bodies.
   b2World world(gravity);
+
+  // Define the ground body.
+  b2BodyDef groundBodyDef;
+  groundBodyDef.position.Set(0.0f, -10.0f);
+// Call the body factory which allocates memory for the ground body
+  // from a pool and creates the ground box shape (also from a pool).
+  // The body is also added to the world.
+  b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+  // Define the ground box shape.
+  b2PolygonShape groundBox;
+
+  // The extents are the half-widths of the box.
+  groundBox.SetAsBox(50.0f, 10.0f);
+
+  // Add the ground fixture to the ground body.
+  groundBody->CreateFixture(&groundBox, 0.0f);
+
 }
 
 void MyApp::update() { }
